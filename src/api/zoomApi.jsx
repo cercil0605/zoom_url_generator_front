ea92c1576d070879692d493ac4ce6d6876351b5f
cercil0605 <kuamaichi2223@gmail.com>
2025-02-05 21:24:02 +0900

    local

--- src/api/zoomApi.jsx
+++ src/api/zoomApi.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const createZoomMeeting = async (data) => {
   try {
-    const response = await axios.post("https://zoomapi.cercil.net/zoom/create_meeting", data,{withCredentials: true});
+    const response = await axios.post("http://localhost:3010/zoom/create_meeting", data,{withCredentials: true});
     return response.data;
   } catch (error) {
     // show error message
--- src/components/OAuthScreen.jsx
+++ src/components/OAuthScreen.jsx
@@ -3,7 +3,7 @@ import "bulma/css/bulma.min.css"
 
 const OAuthScreen = () => {
     const handleOAuthClick = () => {
-        window.location.href = "https://zoomapi.cercil.net/zoom/oauth"
+        window.location.href = "http://localhost:3010/zoom/oauth"
     };
 
     return (
